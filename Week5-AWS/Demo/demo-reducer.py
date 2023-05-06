a6bbd74ef83c8d3ace9f710bf7f3a8e01b3f7c5f
tt_kql <shutong.luo@gmail.com>
2023-05-06 11:41:56 -0700

    add reducer code fix mapper

--- Week5-AWS/Demo/demo-reducer.py
+++ Week5-AWS/Demo/demo-reducer.py
@@ -2,39 +2,109 @@
 
 import sys
 
+
+stopwords = set(["a", "as", "able", "about", "above", "according", "accordingly",
+             "across", "actually", "after", "afterwards", "again", "against", "aint", "all", "allow",
+             "allows", "almost", "alone", "along", "already", "also", "although", "always", "am", "among",
+             "amongst", "an", "and", "another", "any", "anybody", "anyhow", "anyone", "anything", "anyway",
+             "anyways", "anywhere", "apart", "appear","appreciate", "appropriate", "are", "arent", "around",
+             "as", "aside", "ask", "asking", "associated", "at", "available", "away", "awfully", "be", "became",
+             "because", "become", "becomes", "becoming", "been", "before", "beforehand", "behind",
+             "being", "believe", "below", "beside", "besides", "best", "better", "between", "beyond",
+             "both", "brief", "but", "by", "cmon", "cs", "came", "can", "cant", "cannot", "cant",
+             "cause", "causes", "certain", "certainly", "changes", "clearly", "co", "com", "come",
+             "comes", "concerning", "consequently", "consider", "considering", "contain", "containing",
+             "contains", "corresponding", "could", "couldnt", "course", "currently", "definitely",
+             "described", "despite", "did", "didnt", "different", "do", "does", "doesnt", "doing",
+             "dont", "done", "down", "downwards", "during", "each", "edu", "eg", "eight", "either",
+             "else", "elsewhere", "enough", "entirely", "especially", "et", "etc", "even", "ever",
+             "every", "everybody", "everyone", "everything", "everywhere", "ex", "exactly", "example",
+             "except", "far", "few", "ff", "fifth", "first", "five", "followed", "following", "follows",
+             "for", "former", "formerly", "forth", "four", "from", "further", "furthermore", "get",
+             "gets", "getting", "given", "gives", "go", "goes", "going", "gone", "got", "gotten",
+             "greetings", "had", "hadnt", "happens", "hardly", "has", "hasnt", "have", "havent",
+             "having", "he", "hes", "hello", "help", "hence", "her", "here", "heres", "hereafter",
+             "hereby", "herein", "hereupon", "hers", "herself", "hi", "him", "himself",
+             "his", "hither", "hopefully", "how", "howbeit", "however", "i", "id", "ill", "im", "ive",
+             "ie", "if", "ignored", "immediate", "in", "inasmuch", "inc", "indeed", "indicate",
+             "indicated", "indicates", "inner", "insofar", "instead", "into", "inward", "is",
+             "isnt", "it", "itd", "itll", "its", "its", "itself", "just", "keep", "keeps", "kept",
+             "know", "knows", "known", "last", "lately", "later", "latter", "latterly", "least",
+             "less", "lest", "let", "lets", "like", "liked", "likely", "little", "look", "looking",
+             "looks", "ltd", "mainly", "many", "may", "maybe", "me", "mean", "meanwhile", "merely",
+             "might", "more", "moreover", "most", "mostly", "much", "must", "my", "myself",
+             "name", "namely", "nd", "near", "nearly", "necessary", "need", "needs", "neither",
+             "never", "nevertheless", "new", "next", "nine", "no", "nobody", "non", "none", "noone",
+             "nor", "normally", "not", "nothing", "novel", "now", "nowhere", "obviously", "of",
+             "off", "often", "oh", "ok", "okay", "old", "on", "once", "one", "ones", "only",
+             "onto", "or", "other", "others", "otherwise", "ought", "our", "ours", "ourselves",
+             "out", "outside", "over", "overall", "own", "particular", "particularly",
+             "per", "perhaps", "placed", "please", "plus", "possible", "presumably", "probably",
+             "provides", "que", "quite", "qv", "rather", "rd", "re", "really", "reasonably",
+             "regarding", "regardless", "regards", "relatively", "respectively", "right", "said",
+             "same", "saw", "say", "saying", "says", "second", "secondly", "see", "seeing",
+             "seem", "seemed", "seeming", "seems", "seen", "self", "selves", "sensible", "sent",
+             "serious", "seriously", "seven", "several", "shall", "she", "should", "shouldnt",
+             "since", "six", "so", "some", "somebody", "somehow", "someone", "something",
+             "sometime", "sometimes", "somewhat", "somewhere", "soon", "sorry", "specified", "specify",
+             "specifying", "still", "sub", "such", "sup", "sure", "ts", "take", "taken", "tell", "tends",
+             "th", "than", "thank", "thanks", "thanx", "that", "thats", "thats", "the", "their", "theirs",
+             "them", "themselves", "then", "thence", "there", "theres", "thereafter", "thereby",
+             "therefore", "therein", "theres", "thereupon", "these", "they", "theyd",
+             "theyll", "theyre", "theyve", "think", "third", "this", "thorough",
+             "thoroughly", "those", "though", "three", "through", "throughout", "thru",
+             "thus", "to", "together", "too", "took", "toward", "towards", "tried", "tries",
+             "truly", "try", "trying", "twice", "two", "un", "under", "unfortunately",
+             "unless", "unlikely", "until", "unto", "up", "upon", "us", "use", "used",
+             "useful", "uses", "using", "usually", "value", "various", "very", "via", "viz",
+             "vs", "want", "wants", "was", "wasnt", "way", "we", "wed", "well", "were", "weve",
+             "welcome", "well", "went", "were", "werent", "what", "whats", "whatever", "when",
+             "whence", "whenever", "where", "wheres", "whereafter", "whereas", "whereby",
+             "wherein", "whereupon", "wherever", "whether", "which", "while", "whither", "who",
+             "whos", "whoever", "whole", "whom", "whose", "why", "will", "willing", "wish",
+             "with", "within", "without", "wont", "wonder", "would", "would", "wouldnt", "yes",
+             "yet", "you", "youd", "youll", "youre", "youve", "your", "yours", "yourself",
+             "yourselves", "zero"])
+
+def is_valid_token(term):
+    return len(term) > 1 and any(c.isalpha() for c in term) and term not in stopwords
+
 #  Current key being processed, and accumulated counts
 current_docid = None
-current_longest_term = None
-current_count_all = 0
-current_count_filtered = 0
+num_of_words = 0
+num_of_stopword = 0
+num_of_tokens = 0
+num_of_unique_tokens = 0
+token_set = {}
 
 for line in sys.stdin:
     docid, term = line.strip('\n').split('\t')
 
     if current_docid == docid:
-        current_count_all += 1
-        if (len(term) == 0):
-            current_count_filtered += 1
-        if len(term) > len(current_longest_term):
-            current_longest_term = term
+        num_of_words += 1
+        if term in stopwords:
+            num_of_stopword += 1
+        if is_valid_token(term):
+            num_of_tokens += 1
+            token_set.add(term)
+
     else:
         # Seeing either a new different key, or the first one
         if current_docid:
-            print('%s\t%s\t%s\t%s' % (docid,
-                                      current_longest_term,
-                                      len(current_longest_term),
-                                      1.0 * current_count_filtered/current_count_all))
+            print('%s\t%s\t%s\t%s\t%s' % (docid,
+                                      num_of_words,
+                                      num_of_stopword,
+                                      num_of_tokens,
+                                      len(token_set)))
         current_docid = docid
-        current_count_all = 1
-        if len(term) == 0:
-            current_count_filtered = 1
-        else:
-            current_count_filtered = 0
-        current_longest_term = term
-            
-if current_count_all > 0:
-    print('%s\t%s\t%s\t%s' % (docid,
-                              current_longest_term,
-                              len(current_longest_term),
-                              1.0* current_count_filtered/current_count_all))
+        num_of_words = 1
+        num_of_stopword = 0
+        num_of_tokens = 0
+        token_set = ()
+
+        if term in stopwords:
+            num_of_stopword = 1
+        if is_valid_token(term):
+            num_of_tokens = 1
+            token_set.add(term)
 

b7a5a9a31519826a2af8d547696e47b0dfa27cb9
tt_kql <shutong.luo@gmail.com>
2023-05-14 12:34:49 -0700

    create lambda function for id

--- Week6-AWS/Lab/lambda_function_id.py
+++ Week6-AWS/Lab/lambda_function_id.py
@@ -2,14 +2,14 @@ import boto3
 
 # Copied from console version
 
-def create_table(table_name="college2"):
+def create_table(table_name="document-titles"):
     return boto3.resource('dynamodb').Table(table_name)
 
-def state_and_cost_range_query(table, state, low_cost, high_cost):
+def state_and_cost_range_query(table, id):
     query_params = {
-        'KeyConditionExpression': 'state_code = :sc and out_of_state_total between :minc and :maxc',
-        'ProjectionExpression': "state_code, out_of_state_total",
-        'ExpressionAttributeValues': {':sc': state, ':minc': low_cost, ':maxc': high_cost } }
+        'KeyConditionExpression': 'id = :id',
+        'ProjectionExpression': "id, title",
+        'ExpressionAttributeValues': {':id': id} }
     response = table.query(**query_params)
     return response['Items']
     
@@ -18,25 +18,16 @@ def state_and_cost_range_query(table, state, low_cost, high_cost):
 #  Convention is the handler is named lambda_handler and the file is named lambda_function
 
 def lambda_handler(event, context):
-    statecode = event['queryStringParameters']['statecode']
-    lowcost = int(event['queryStringParameters']['lowcost'])
-    highcost = int(event['queryStringParameters']['highcost'])
-    items = state_and_cost_range_query(create_table(),statecode, lowcost, highcost)
-    return format_html_query_results(statecode, lowcost, highcost, items)
+    id = event['queryStringParameters']['id']
+    items = state_and_cost_range_query(create_table(),id)
+    return format_html_query_results(id, items)
 
 # Generate a full HTML page from a list of items returned by the search
-def format_html_query_results(statecode, lowcost, highcost, items):
-    banner_html = f"<h3>Search results for state {statecode}<br/>Low cost is {lowcost}<br/>High cost is {highcost}</h3><p/>"
+def format_html_query_results(id, items):
+    banner_html = f"<h3>Search results for id {id}</h3><p/>"
     item_html = '<ol>'
     for item in items:
-        item_html += f"<li>{item['state_code']}, {item['out_of_state_total']}</li>"
+        item_html += f"<li>{item['id']}, {item['title']}</li>"
     item_html += '</ol>'
     html = f"<html><body>{banner_html} {item_html}</body></html>"    
     return {'statusCode': 200, 'headers': {'Content-Type': 'text/html'},'body': html}
-
-
-
-
-
-
-

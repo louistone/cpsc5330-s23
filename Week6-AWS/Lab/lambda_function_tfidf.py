158f2dfdd65a8f3faea196e29e28d185e97f80d6
tt_kql <shutong.luo@gmail.com>
2023-05-14 18:00:47 -0700

    table is used as global parameter

--- Week6-AWS/Lab/lambda_function_tfidf.py
+++ Week6-AWS/Lab/lambda_function_tfidf.py
@@ -2,32 +2,41 @@ import boto3
 
 # Copied from console version
 
-def create_table(table_name="term_fre"):
+def create_table(table_name="tfidf"):
     return boto3.resource('dynamodb').Table(table_name)
 
-def state_and_cost_range_query(table, term):
-    query_params = {
-        'KeyConditionExpression': 'term = :term',
-        'ProjectionExpression': "term, fre, id",
-        'ExpressionAttributeValues': {':term': term} }
-    response = table.query(**query_params)
-    return response['Items']
-    
+tfidf_table = create_table()
 
+def get_tfidf(term, docid):
+    query_params = {
+        'KeyConditionExpression': 'term = :term and id = :docid',
+        'ProjectionExpression': "term, id, fre",
+        'ExpressionAttributeValues': {':term': term,':docid': docid} }
+    response = tfidf_table.query(**query_params)
+    items = response['Items']
+    if items:
+        print(items)
+        return items[0]['fre']
+    else:
+        return 0.0
+        
 #  Lambda-specific code
 #  Convention is the handler is named lambda_handler and the file is named lambda_function
 
 def lambda_handler(event, context):
     term = event['queryStringParameters']['term']
-    items = state_and_cost_range_query(create_table(),term)
-    return format_html_query_results(id, items)
+    #items = state_and_cost_range_query(create_table(),term)
+    docid = event['queryStringParameters']['docid']
+    tfidf = get_tfidf(term, docid)
+    return format_html_query_results(term,docid,tfidf)
 
 # Generate a full HTML page from a list of items returned by the search
-def format_html_query_results(id, items):
-    banner_html = f"<h3>Search results for id {id}</h3><p/>"
-    item_html = '<ol>'
-    for item in items:
-        item_html += f"<li>{item['term']}, {item['fre']}, {item['id']}</li>"
-    item_html += '</ol>'
+def format_html_query_results(term,docid,tfidf):
+    banner_html = f"<h3>Search results for term {term} and docid {docid}</h3><p/>"
+    item_html = f"<h3>tfidf {tfidf}</h3><p/>"
+    # item_html = '<ol>'
+    # for item in items:
+    #     item_html += f"<li>{item['term']}, {item['fre']}, {item['id']}</li>"
+    # item_html += '</ol>'
     html = f"<html><body>{banner_html} {item_html}</body></html>"    
     return {'statusCode': 200, 'headers': {'Content-Type': 'text/html'},'body': html}
